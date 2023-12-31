6816b1f58553ef138854f19352bc0be3a50b0f78
Srinath6 <57441534+Srinath6@users.noreply.github.com>
2024-01-01 00:39:24 +0530

    feat: test 3 auto tests

--- auto-tests/src/App.test.js
+++ auto-tests/src/App.test.js
@@ -14,7 +14,7 @@ describe("App", () => {
             {
               userId: 1,
               id: 1,
-              title: "Kaliteye hoşgeldin",
+              title: "Kaliteye hoşge",
               completed: false,
             },
           ]),
